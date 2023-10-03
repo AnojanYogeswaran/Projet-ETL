7d55ea69332ddaa6fd16e75b873bd1c65b1c2d95
Anojan Yogeswaran <ayogeswaran@netwo.com>
2023-10-03 16:27:52 +0200

    question1.4

--- qu4.py
+++ qu4.py
@@ -9,27 +9,38 @@ fichier_csv = sys.argv[1]
 
 # Étape 1 : Compter le nombre de produits par magasin dans la catégorie 5
 magasin_produits = {}
-top_10_magasins = [60,64,61,65,69,68,66,63,62,67]
+magasin_produits_176 = {}
+top_10_magasins = ['55','65','51','58','76','71','74','75','72','59']
+somme = 0
 
 with open(fichier_csv, newline='') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
-        if len(row) > 4:  # Assurez-vous qu'il y a au moins 5 colonnes
-            fabricant = row[1]
-            categorie = row[2]
-            magasin = row[4]
-
-            if categorie == '5' and magasin in top_10_magasins:
-                if magasin in magasin_produits:
-                    magasin_produits[magasin] += 1
+        produit = row[1]
+        categorie = row[2]
+        magasin = row[4]
+        if categorie == '5' and magasin in top_10_magasins:
+            if magasin in magasin_produits:
+                magasin_produits[magasin] += 1
+            else:
+                magasin_produits[magasin] = 1
+        if row[3] == '707' and magasin in top_10_magasins:
+                if magasin in magasin_produits_176:
+                    magasin_produits_176[magasin] += 1
                 else:
-                    magasin_produits[magasin] = 1
+                    magasin_produits_176[magasin] = 1
+            
+
 
 # Étape 2 : Compter le nombre total de produits dans la catégorie 5 vendus par les magasins du top 10
-nombre_total_produits_cat5 = sum(magasin_produits.values())
 
+print(magasin_produits)
+print(magasin_produits_176)
 # Étape 3 : Calculer la part du nombre de produits offerts par le fabricant 1664
-fabricant_1664_produits = sum(magasin_produits[magasin] for magasin in magasin_produits if 1664 in magasin)
-part_fabricant_1664 = fabricant_1664_produits / nombre_total_produits_cat5
+for cle, valeur in magasin_produits.items():
+    print(f'Clé : {magasin_produits[cle]}, Valeur : {magasin_produits_176[cle]}')
+    somme = somme + (magasin_produits_176[cle]/magasin_produits[cle])*100
+print(somme)
 
-print(f"La part du nombre de produits offerts par le fabricant 1664 dans l'ensemble des produits de la catégorie 5 vendus dans les magasins du top 10 est de : {part_fabricant_1664:.2%}")
+
+print(f"La part du nombre de produits offerts par le fabricant 1664 dans l'ensemble des produits de la catégorie 5 vendus dans les magasins du top 10 est de : {somme/10}")
